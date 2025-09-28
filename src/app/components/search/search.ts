6bbd44c15e2c2c7f53a400e21bf6b38338b31c07
Anton <anton.m.lotos@gmail.com>
2025-09-28 16:59:18 +0300

    feat(components): create header, search, sidebar components

---
+++ src/app/components/search/search.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  standalone: false,
+  templateUrl: './search.html',
+  styleUrl: './search.css'
+})
+export class Search {
+
+}
