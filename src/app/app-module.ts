684dbffdcfd5d7eb156bcaabcb0f4c54cdd15486
Anton <anton.m.lotos@gmail.com>
2025-09-28 16:14:02 +0300

    feat(components): initialize components folder

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Card } from './components/card/card';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Card
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card/card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: false,
+  templateUrl: './card.html',
+  styleUrl: './card.css'
+})
+export class Card {
+
+}
