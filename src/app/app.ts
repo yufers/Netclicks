4f5c0a282face5f8fb86b49bdc79c609c77e561f
Anton <anton.m.lotos@gmail.com>
2025-09-28 16:03:46 +0300

    feat(src/app): update .editconfig, update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,6 @@ import { Component, signal } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('Netclicks-app');
-}
+export class App {}
