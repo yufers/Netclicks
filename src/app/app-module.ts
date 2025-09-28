749d0f8113e927b5a4ea3e33cf8ca68bb66cefab
Anton <anton.m.lotos@gmail.com>
2025-09-28 17:12:49 +0300

    chore(app): import and declarate new components in app-modules.ts

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Card } from './components/card/card';
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Search } from './components/search/search';
 
 @NgModule({
-  declarations: [App, Card],
+  declarations: [App, Card, Header, Sidebar, Search],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
