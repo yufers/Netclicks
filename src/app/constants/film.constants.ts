ecd98d375adad165b941ad820fe5489804326a34
Anton <anton.m.lotos@gmail.com>
2025-09-29 20:46:12 +0300

    feat(constants): add film constant

---
+++ src/app/constants/film.constants.ts
@@ -0,0 +1,31 @@
+import { Film } from "../models/film.model";
+
+export const FILMS: Film[] = [
+  {
+    id: 0,
+    title: 'Марсианин 1',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: './film.html',
+    image: './images/film.png',
+  },
+  {
+    id: 1,
+    title: 'Марсианин 2',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: './film.html',
+    image: './images/film.png',
+  },
+  {
+    id: 2,
+    title: 'Марсианин 3',
+    original: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: './film.html',
+    image: './images/film.png',
+  },
+];
