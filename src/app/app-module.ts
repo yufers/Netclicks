4116ec80430254a85553598196d4d10f239fc224
Anton <anton.m.lotos@gmail.com>
2025-09-29 22:23:20 +0300

    refactor(app): add router-outlet tag, add new routes, declarate catalog and film page

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,9 +7,11 @@ import { Card } from './components/card/card';
 import { Header } from './components/header/header';
 import { Sidebar } from './components/sidebar/sidebar';
 import { Search } from './components/search/search';
+import { CatalogPage } from './pages/catalog-page/catalog-page';
+import { FilmPage } from './pages/film-page/film-page';
 
 @NgModule({
-  declarations: [App, Card, Header, Sidebar, Search],
+  declarations: [App, Card, Header, Sidebar, Search, CatalogPage, FilmPage],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogPage } from './pages/catalog-page/catalog-page';
+import { FilmPage } from './pages/film-page/film-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CatalogPage,
+  },
+  {
+    path: 'film',
+    component: FilmPage,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
