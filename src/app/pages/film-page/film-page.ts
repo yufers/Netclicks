7ed0488d6a4d0124ad389e660afbd8bf6ef71484
Anton <anton.m.lotos@gmail.com>
2025-09-29 22:18:25 +0300

    feat(film-page): add film-page

---
+++ src/app/pages/film-page/film-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-page',
+  standalone: false,
+  templateUrl: './film-page.html',
+  styleUrl: './film-page.css'
+})
+export class FilmPage {
+
+}
