610f575b092f0c96979c5c4d8e28f2e78d5a2a08
Anton <anton.m.lotos@gmail.com>
2025-09-29 22:19:06 +0300

    refactor(constants): update links

--- src/app/constants/film.constants.ts
+++ src/app/constants/film.constants.ts
@@ -7,7 +7,7 @@ export const FILMS: Film[] = [
     original: 'The Martian 2015',
     category: 'Фантастика, приключения',
     rating: 7.8,
-    link: './film.html',
+    link: './film',
     image: './images/film.png',
   },
   {
@@ -16,7 +16,7 @@ export const FILMS: Film[] = [
     original: 'The Martian 2015',
     category: 'Фантастика, приключения',
     rating: 7.8,
-    link: './film.html',
+    link: './film',
     image: './images/film.png',
   },
   {
@@ -25,7 +25,7 @@ export const FILMS: Film[] = [
     original: 'The Martian 2015',
     category: 'Фантастика, приключения',
     rating: 7.8,
-    link: './film.html',
+    link: './film',
     image: './images/film.png',
   },
 ];
