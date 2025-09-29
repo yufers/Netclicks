98eab8720c677717bfcb7e4e593d4609338b2417
Anton <anton.m.lotos@gmail.com>
2025-09-29 21:52:25 +0300

    refactor(card): add constants visualization

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Film } from '../../models/film.model';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +8,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './card.css',
 })
 export class Card {
-  @Input() title: string = '';
+  @Input() film!: Film;
 }
