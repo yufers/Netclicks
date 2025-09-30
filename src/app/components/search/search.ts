c3c649cd658b5e0bd2de0cf8873d9b417394dd85
Anton <anton.m.lotos@gmail.com>
2025-09-30 15:01:14 +0300

    feat(search): update search logic

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FilmsService } from '../../services/films.service';
 
 @Component({
   selector: 'app-search',
   standalone: false,
   templateUrl: './search.html',
-  styleUrl: './search.css'
+  styleUrl: './search.css',
 })
 export class Search {
+  public searchString: string = '';
 
+  constructor(private _filmsService: FilmsService) {}
+
+  public updateValue(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.searchString = target.value
+  }
+
+  public updateSearch(event: Event) {
+    this._filmsService.searchString = this.searchString;
+  }
 }
