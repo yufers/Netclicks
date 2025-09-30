4970bd69ed5f4e80809545711145dc116d89d0fc
Anton <anton.m.lotos@gmail.com>
2025-09-30 13:52:48 +0300

    feat(film-page): add page logic

--- src/app/pages/film-page/film-page.ts
+++ src/app/pages/film-page/film-page.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Film } from '../../models/film.model';
+import { FILMS } from '../../constants/film.constants';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-film-page',
   standalone: false,
   templateUrl: './film-page.html',
-  styleUrl: './film-page.css'
+  styleUrl: './film-page.css',
 })
-export class FilmPage {
+export class FilmPage implements OnInit {
+  public films: Film[] = FILMS;
+  public film!: Film;
 
+  constructor(private _route: ActivatedRoute, private _router: Router) {}
+
+  ngOnInit(): void {
+    const id = this._route.snapshot.paramMap.get('id');
+
+    this._initFilm(Number(id));
+  }
+
+  private _initFilm(id: number) {
+    const findFilm = this.films.find((film) => film.id === id);
+
+    if (findFilm) {
+      this.film = findFilm;
+    } else {
+      this._router.navigate(['/']);
+    }
+  }
 }
